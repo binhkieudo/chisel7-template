495f22ad47f9b33dbf1552a52dc9b85e8205da59
binhkieudo <binhkieudo@gmail.com>
2026-01-12 16:02:53 +0900

    Update vcd/fst wave generation

--- src/test/scala/gcd/GCDSpec.scala
+++ src/test/scala/gcd/GCDSpec.scala
@@ -8,25 +8,28 @@ import chisel3.simulator.scalatest.ChiselSim
 import org.scalatest.freespec.AnyFreeSpec
 import org.scalatest.matchers.must.Matchers
 
-/**
-  * This is a trivial example of how to run this Specification
-  * From within sbt use:
-  * {{{
-  * testOnly gcd.GCDSpec
-  * }}}
-  * From a terminal shell use:
-  * {{{
-  * sbt 'testOnly gcd.GCDSpec'
-  * }}}
-  * Testing from mill:
-  * {{{
-  * mill %NAME%.test.testOnly gcd.GCDSpec
-  * }}}
-  */
+import svsim._
+import svsim.verilator.Backend._
+import svsim.verilator.Backend.CompilationSettings._
+
 class GCDSpec extends AnyFreeSpec with Matchers with ChiselSim {
+
+  implicit val customVerilatorSettings: BackendSettingsModifications = (currentSettings: svsim.Backend.Settings) => {
+    currentSettings match {
+      case currentSettings: CompilationSettings =>
+        val modifiedVerilatorSettings = currentSettings.copy(
+          //traceStyle = Some(CompilationSettings.TraceStyle(kind = TraceKind.Vcd, traceUnderscore = false))
+          traceStyle = Some(CompilationSettings.TraceStyle(kind = TraceKind.Fst(traceThreads = Some(16)), traceUnderscore = false))
+        )
+    modifiedVerilatorSettings
+      case otherSettings => otherSettings
+    }
+  }
 
   "Gcd should calculate proper greatest common denominator" in {
     simulate(new DecoupledGcd(16)) { dut =>
+      enableWaves()
+
       val testValues = for { x <- 0 to 10; y <- 0 to 10} yield (x, y)
       val inputSeq = testValues.map { case (x, y) => (new GcdInputBundle(16)).Lit(_.value1 -> x.U, _.value2 -> y.U) }
       val resultSeq = testValues.map { case (x, y) =>
